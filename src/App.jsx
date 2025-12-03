0683b123ad40fbcdd43758c61e4e9033807e42a2
chuotchuonchetra <tranevercare2504@gmail.com>
2025-12-03 14:13:43 +0700

    updatelink to match the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import CheckOutPage from "./pages/CheckoutPage";
 import HomePage from "./pages/HomePage";
 import { Route, Routes } from "react-router";
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
+      <Route path="checkout" element={<CheckOutPage />} />
     </Routes>
   );
 }
