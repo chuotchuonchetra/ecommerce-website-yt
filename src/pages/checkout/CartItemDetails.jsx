5e8fe7d90896ae5d8df81ff338aecb718994a5e3
chuotchuonchetra <tranevercare2504@gmail.com>
2025-12-11 21:39:08 +0700

    create delete cart item feature

--- src/pages/checkout/CartItemDetails.jsx
+++ src/pages/checkout/CartItemDetails.jsx
@@ -1,6 +1,11 @@
+import axios from "axios";
 import { formatMoney } from "../../utils/money";
 
-export function CartItemDetails({ cartItem }) {
+export function CartItemDetails({ cartItem, loadCart }) {
+  const deleteCartItem = async () => {
+    await axios.delete(`/api/cart-items/${cartItem.productId}`);
+    await loadCart();
+  };
   return (
     <>
       <div className="cart-item-details">
@@ -8,13 +13,22 @@ export function CartItemDetails({ cartItem }) {
         <div className="product-price">
           {formatMoney(cartItem.product.priceCents)}
         </div>
-        <div className="product-quantity">
+        <div className="product-quantity flex">
           <span>
             Quantity:{" "}
             <span className="quantity-label">{cartItem.quantity}</span>
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
-          <span className="delete-quantity-link link-primary">Delete</span>
+          <span
+            className="update-quantity-link link-primary"
+            style={{ cursor: "pointer" }}>
+            Update
+          </span>
+          <span
+            className="delete-quantity-link link-primary"
+            style={{ cursor: "pointer" }}
+            onClick={deleteCartItem}>
+            Delete
+          </span>
         </div>
       </div>
     </>
